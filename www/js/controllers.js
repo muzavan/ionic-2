07b7eb975536c049d35304772bff8a4595c8bd1b
tegarajipangestu <13512061@std.stei.itb.ac.id>
2015-11-05 02:06:40 +0700

    Fix cache issue

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -47,7 +47,6 @@ angular.module('starter.controllers', [])
 
 .controller('CalonCtrl',function($scope){
   if(window.localStorage.getItem("sp_lokasi")!=null){
-    window.localStorage.clear();
     if(window.localStorage.getItem("calon")==null){
       //console.log("Via Api");
       var ajaxUrl = API_ROOT_URL+"calonpilkada/api/candidates?apiKey="+API_KEY;
@@ -61,13 +60,16 @@ angular.module('starter.controllers', [])
           // console.log(mData[i]);
           for (j in [0,1])
           {
-            var newItem = '<li class="item" style="white-space:normal;box-shadow: 1px 2px 1px #aaaaaa;margin-bottom:5px;text-align:center;"><h3 style="white-space:normal;font-weight:700;font-size:14pt;">'+mData[i].paslon[j].nama+'</h3><p style="white-space:normal;">'+toTitleCase(mData[i].paslon[j].alamat)+'</p><p style="margin-top:3px;"><b style="font-size:11pt;">Pekerjaan</b></p><p style="white-space:normal;">'+toTitleCase(mData[i].paslon[j].pekerjaan)+'</p><p style="margin-top:3px;"><b style="font-size:10pt;">Partai Pendukung</b></p><p style="white-space:normal;">'+mData[i].dukungan+'</p><p style="margin-top:3px;"><b style="font-size:10pt;">Tempat tanggal lahir</b></p><p style="white-space:normal;">'+toTitleCase(mData[i].paslon[j].pob)+' '+mData[i].paslon[j].dob+'</p></li>';
-            $("#calons").append(newItem);          
+            if (mData[i].daerah.id==JSON.parse(window.localStorage.getItem("sp_lokasi")).id)
+            {
+              var newItem = '<li class="item" style="white-space:normal;box-shadow: 1px 2px 1px #aaaaaa;margin-bottom:5px;text-align:center;"><h3 style="white-space:normal;font-weight:700;font-size:14pt;">'+mData[i].paslon[j].nama+'</h3><p style="white-space:normal;">'+toTitleCase(mData[i].paslon[j].alamat)+'</p><p style="margin-top:3px;"><b style="font-size:11pt;">Pekerjaan</b></p><p style="white-space:normal;">'+toTitleCase(mData[i].paslon[j].pekerjaan)+'</p><p style="margin-top:3px;"><b style="font-size:10pt;">Partai Pendukung</b></p><p style="white-space:normal;">'+mData[i].dukungan+'</p><p style="margin-top:3px;"><b style="font-size:10pt;">Tempat tanggal lahir</b></p><p style="white-space:normal;">'+toTitleCase(mData[i].paslon[j].pob)+' '+mData[i].paslon[j].dob+'</p></li>';
+              $("#calons").append(newItem);                        
+            }
           }
         }
       });
-    }
-    else{
+}
+else{
   //    console.log("Via Local");
       //console.log(window.localStorage.getItem("partai"));
       var mData = JSON.parse(window.localStorage.getItem("calon"));
@@ -75,16 +77,19 @@ angular.module('starter.controllers', [])
           // console.log(mData[i]);
           for (j in [0,1])
           {
-            var newItem = '<li class="item" style="white-space:normal;box-shadow: 1px 2px 1px #aaaaaa;margin-bottom:5px;text-align:center;"><h3 style="white-space:normal;font-weight:700;font-size:14pt;">'+mData[i].paslon[j].nama+'</h3><p style="white-space:normal;">'+toTitleCase(mData[i].paslon[j].alamat)+'</p><p style="margin-top:3px;"><b style="font-size:11pt;">Pekerjaan</b></p><p style="white-space:normal;">'+mData[i].paslon[j].pekerjaan+'</p><p style="margin-top:3px;"><b style="font-size:10pt;">Partai Pendukung</b></p><p style="white-space:normal;">'+mData[i].dukungan+'</p><p style="margin-top:3px;"><b style="font-size:10pt;">Tempat tanggal lahir</b></p><p style="white-space:normal;">'+toTitleCase(mData[i].paslon[j].pob)+' '+mData[i].paslon[j].dob+'</p></li>';
-            $("#calons").append(newItem);          
+            if (mData[i].daerah.id==JSON.parse(window.localStorage.getItem("sp_lokasi")).id)
+            {
+              var newItem = '<li class="item" style="white-space:normal;box-shadow: 1px 2px 1px #aaaaaa;margin-bottom:5px;text-align:center;"><h3 style="white-space:normal;font-weight:700;font-size:14pt;">'+mData[i].paslon[j].nama+'</h3><p style="white-space:normal;">'+toTitleCase(mData[i].paslon[j].alamat)+'</p><p style="margin-top:3px;"><b style="font-size:11pt;">Pekerjaan</b></p><p style="white-space:normal;">'+toTitleCase(mData[i].paslon[j].pekerjaan)+'</p><p style="margin-top:3px;"><b style="font-size:10pt;">Partai Pendukung</b></p><p style="white-space:normal;">'+mData[i].dukungan+'</p><p style="margin-top:3px;"><b style="font-size:10pt;">Tempat tanggal lahir</b></p><p style="white-space:normal;">'+toTitleCase(mData[i].paslon[j].pob)+' '+mData[i].paslon[j].dob+'</p></li>';
+              $("#calons").append(newItem);                        
+            }
           }
-      }
-    }  
-  }
-  else{
-    $("#calons").append("Anda belum mengatur lokasi anda. Atur lokasi dengan pergi ke bagian Pengaturan >> Pilih Lokasi >> Simpan Lokasi");
-  }
-})
+        }
+      }  
+    }
+    else{
+      $("#calons").append("Anda belum mengatur lokasi anda. Atur lokasi dengan pergi ke bagian Pengaturan >> Pilih Lokasi >> Simpan Lokasi");
+    }
+  })
 .controller('PartaiCtrl',function($scope){
   if(window.localStorage.getItem("sp_lokasi")!=null){
     if(window.localStorage.getItem("partai")==null){
@@ -94,7 +99,7 @@ angular.module('starter.controllers', [])
       $.get(ajaxUrl,{},function(data){
         //console.log(data);
         var mData = data.data.results.parties;
-        console.log(mData);
+        // console.log(mData);
         window.localStorage.setItem("partai",JSON.stringify(mData));
         for(i in mData){
           // console.log(mData[i]);
@@ -111,13 +116,13 @@ angular.module('starter.controllers', [])
           // console.log(mData[i]);
           var newItem = '<li class="item" style="white-space:normal;box-shadow: 1px 2px 1px #aaaaaa;margin-bottom:5px;text-align:center;"><h3 style="white-space:normal;font-weight:700;font-size:14pt;">'+mData[i].nama_lengkap+'</h3><img src="'+mData[i].url_logo_medium+'" height="60px" width="60px" /><p style="margin-top:3px;"><b style="font-size:11pt;">Visi</b></p><p style="white-space:normal;">'+mData[i].visi+'</p><p style="margin-top:3px;"><b style="font-size:10pt;">Misi</b></p><p style="white-space:normal;">'+mData[i].misi+'</p><p style="margin-top:3px;"><b style="font-size:10pt;">Program</b></p><p style="white-space:normal;">'+mData[i].program+'</p></li>';
           $("#partais").append(newItem);
+        }
       }
     }
-  }
-  else{
-    $("#partais").append("Anda belum mengatur lokasi anda. Atur lokasi dengan pergi ke bagian Pengaturan >> Pilih Lokasi >> Simpan Lokasi");
-  }
-})
+    else{
+      $("#partais").append("Anda belum mengatur lokasi anda. Atur lokasi dengan pergi ke bagian Pengaturan >> Pilih Lokasi >> Simpan Lokasi");
+    }
+  })
 .controller('SengketaCtrl',function($scope){
   if(window.localStorage.getItem("sp_lokasi")!=null){
 
@@ -135,19 +140,23 @@ angular.module('starter.controllers', [])
       $.get(ajaxUrl,{},function(data){
         //console.log(data);
         var mData = data.data.results.anggaran;
-        console.log(mData);
+        // console.log(mData);
         window.localStorage.setItem("anggaran",JSON.stringify(mData));
         for(i in mData){
           // console.log(mData[i]);
-          var diajukan = "Rp "+mData[i].diajukan.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-          var disetujui = "Rp "+mData[i].disetujui.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-          var digunakan = "Rp "+mData[i].digunakan.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-          var newItem = '<li class="item" style="white-space:normal;box-shadow: 1px 2px 1px #aaaaaa;margin-bottom:5px;"><h3 style="white-space:normal;">'+mData[i].wilayah.nama+'</h3><p style="font-size:9pt;font-weight:700;white-space:normal;">Diajukan : '+diajukan+'</p><p style="white-space:normal;">Disetujui :'+disetujui+'</p><p style="white-space:normal;">Digunakan :'+digunakan+'</p></li>';
-          $("#anggarans").append(newItem);
+          if (JSON.parse(window.localStorage.getItem("sp_lokasi")).id==mData[i].wilayah.id)
+          {
+            var diajukan = "Rp "+mData[i].diajukan.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+            var disetujui = "Rp "+mData[i].disetujui.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+            var digunakan = "Rp "+mData[i].digunakan.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+            var newItem = '<li class="item" style="white-space:normal;box-shadow: 1px 2px 1px #aaaaaa;margin-bottom:5px;"><h3 style="white-space:normal;">'+mData[i].wilayah.nama+'</h3><p style="font-size:9pt;font-weight:700;white-space:normal;">Diajukan : '+diajukan+'</p><p style="white-space:normal;">Disetujui :'+disetujui+'</p><p style="white-space:normal;">Digunakan :'+digunakan+'</p></li>';
+            $("#anggarans").append(newItem);            
+            break;
+          }
         }
       });
-    }
-    else{
+}
+else{
       //console.log("Via Local");
       //console.log(window.localStorage.getItem("anggaran"));
       var mData = JSON.parse(window.localStorage["anggaran"]);
@@ -156,8 +165,16 @@ angular.module('starter.controllers', [])
         var diajukan = "Rp "+mData[i].diajukan.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
         var disetujui = "Rp "+mData[i].disetujui.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
         var digunakan = "Rp "+mData[i].digunakan.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-        var newItem = '<li class="item" style="white-space:normal;box-shadow: 1px 2px 1px #aaaaaa;margin-bottom:5px;"><h3 style="white-space:normal;">'+mData[i].wilayah.nama+'</h3><p style="font-size:9pt;font-weight:700;white-space:normal;">Diajukan : '+diajukan+'</p><p style="white-space:normal;">Disetujui :'+disetujui+'</p><p style="white-space:normal;">Digunakan :'+digunakan+'</p></li>';
-        $("#anggarans").append(newItem);
+          console.log(window.localStorage.getItem("sp_lokasi"));
+          if (JSON.parse(window.localStorage.getItem("sp_lokasi")).id==mData[i].wilayah.id)
+          {
+            var diajukan = "Rp "+mData[i].diajukan.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+            var disetujui = "Rp "+mData[i].disetujui.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+            var digunakan = "Rp "+mData[i].digunakan.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+            var newItem = '<li class="item" style="white-space:normal;box-shadow: 1px 2px 1px #aaaaaa;margin-bottom:5px;"><h3 style="white-space:normal;">'+mData[i].wilayah.nama+'</h3><p style="font-size:9pt;font-weight:700;white-space:normal;">Diajukan : '+diajukan+'</p><p style="white-space:normal;">Disetujui :'+disetujui+'</p><p style="white-space:normal;">Digunakan :'+digunakan+'</p></li>';
+            $("#anggarans").append(newItem);            
+            break;
+          }
       }
     }
   }
@@ -180,8 +197,8 @@ angular.module('starter.controllers', [])
         $("#faqs").append(newItem);
       }
     });
-    }
-    else{
+  }
+  else{
       //console.log("Via Local");
       //console.log(window.localStorage["faq"]);
       var mData = JSON.parse(window.localStorage.getItem("faq"));
@@ -189,50 +206,49 @@ angular.module('starter.controllers', [])
           // console.log(mData[i]);
           var newItem = '<li class="item" style="white-space:normal;box-shadow: 1px 2px 1px #aaaaaa;margin-bottom:5px;"><h3 style="white-space:normal;">'+mData[i].question_text+'</h3><p style="font-size:9pt;font-weight:700;white-space:normal;">'+mData[i].relevant_laws_regulations+'</p><p style="white-space:normal;">'+mData[i].question_answer+'</p></li>';
           $("#faqs").append(newItem);
+        }
       }
     }
-  }
-  else{
-    $("#faqs").append("Anda belum mengatur lokasi anda. Atur lokasi dengan pergi ke bagian Pengaturan >> Pilih Lokasi >> Simpan Lokasi");
-  } 
-})
+    else{
+      $("#faqs").append("Anda belum mengatur lokasi anda. Atur lokasi dengan pergi ke bagian Pengaturan >> Pilih Lokasi >> Simpan Lokasi");
+    } 
+  })
 .controller('PengaturanCtrl',function($scope){
-    var regionsURL = "js/regions.json";
-    var mData = null;
-    $.get(regionsURL,{},function(data){
-      mData = data;
-      for(i in mData){
-        var newItem = '<option value="'+mData[i].id+'">'+mData[i].name+'</option>';
-        $("#lokasis").append(newItem);
-      }
-    });
+  var regionsURL = "js/regions.json";
+  var mData = null;
+  $.get(regionsURL,{},function(data){
+    mData = data;
+    for(i in mData){
+      var newItem = '<option value="'+mData[i].id+'">'+mData[i].name+'</option>';
+      $("#lokasis").append(newItem);
+    }
+  });
 
-    $scope.ubahLokasi = function(_val){
-      $scope.lokasi = _val;
-    };
+  $scope.ubahLokasi = function(_val){
+    $scope.lokasi = _val;
+  };
 
-    $scope.simpanLokasi = function(){
-      var val = $scope.lokasi;
-      console.log("Simpan Lokasi:",val);
-      for(i in mData){
-        if(mData[i].id == val){
+  $scope.simpanLokasi = function(){
+    var val = $scope.lokasi;
+    console.log("Simpan Lokasi:",val);
+    for(i in mData){
+      if(mData[i].id == val){
           //console.log(mData[i].id,mData[i].name);
-          window.localStorage.clear();
           window.localStorage.setItem("sp_lokasi",JSON.stringify(mData[i]));
           $("#pesan").text("Berhasil disimpan.");
           break;
         }
       }
     };
-});
+  });
 
 function toTitleCase(str)
 {
-    if (str === str.toUpperCase())
-    {
-      return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});      
-    }
-    else
-      return str
+  if (str === str.toUpperCase())
+  {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});      
+  }
+  else
+    return str
 }
 
